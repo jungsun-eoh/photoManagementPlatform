cc80cc86992dde27de68c3f0e5e74fc38e3a447d
jungsun-eoh <jungsun.eoh@gmail.com>
2020-05-19 23:13:05 -0700

    login

--- application/routes/users.js
+++ application/routes/users.js
@@ -1,12 +1,14 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
 const errorPrint = require('../helpers/debug/debughelpers').errorPrint;
 const successPrint = require('../helpers/debug/debughelpers').successPrint;
 const UserError = require('../helpers/errors/UserError');
+
 const db = require('../config/database');
 
 router.post('/register', (req, resp, next) => {
+
   let username = req.body.username;
   let email = req.body.email;
   let password=req.body.password;
@@ -19,30 +21,31 @@ router.post('/register', (req, resp, next) => {
    * check password
    * 
    */
-    db.execute('SELECT * FROM users WHERE userename=?', [username])
+    db.execute('SELECT * FROM users WHERE username=?', [username])
     // breaking promise chain
       .then(([results, fields]) => {
         if(results && results.length == 0) {
-          return db.execute('SELECT * FROM users WHERE email=?', [email])
+          return db.execute('SELECT * FROM users WHERE email=?', [email]);
         } else {
-          throw new UserError('username already exists', '/register', 200);
+          throw new UserError(
+            'username already exists', '/register', 200);
         }
       })
       .then (([results, fields]) => {
         if(results && results.length == 0) {
-          return db.execute(
-            'INSERT INTO users (username, email, password, created) VALUES (?,?,?), now();', 
-            [username, email, password]);
+          let baseSQL = 
+            'INSERT INTO users (username, email, password, created) VALUES (?,?,?, now());';
+          return db.execute(baseSQL, [username, email, password]);
         } else {
-          throw new UserError('emailalready exists', '/register', 200);
+          throw new UserError('email already exists', '/register', 200);
         }
       })
       .then (([results, fields]) => {
         if(results && results.affectedRows) {
           successPrint('user has been created');
-          resp.redirect('login');
+          resp.redirect('/login');
         } else {
-          throw new UserError('Server Error, fail to create new user', "register", 500);
+          throw new UserError('Server Error, fail to create new user', '/register', 500);
           
         }
       })
@@ -51,8 +54,10 @@ router.post('/register', (req, resp, next) => {
           errorPrint(err.getMessage());
           resp.status(err.getStatus());
           resp.redirect(err.getRedirectURL);
-        }
-      })
+        } 
+          next(err);
+        
+      });
    //make sure username donsnt exist
    // make usre email doesnt exit
    // create new user
@@ -62,8 +67,32 @@ router.post('/register', (req, resp, next) => {
       // if usererror
       // set status and message and rediriect
 
-})
+});
 
+router.post('/login', (req, resp, next) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  db.execute('SELECT * FROM users WHERE username=? AND password=?', [username, password])
+    .then(([results, fields])=> {
+      if (results && results.length == 1) {
+        successPrint('successful login');
+        resp.redirect('/');
+      } else {
+        throw new UserError('username or password is incorrect', '/login', 200);
+      }
+    })
+    .catch((err) => {
+      if(err instanceof UserError) {
+        errorPrint(err.getMessage());
+        resp.status(err.getStatus());
+        resp.redirect(err.getRedirectURL());
+      } else {
+        next(err);
+      }
+    })
+
+});
 /* GET users listing. */
 
 // //localhost:3000/users
